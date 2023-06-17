a81fdeab7b55718cf9f2286bf49d61b8fe994c7c
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-06-17 13:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
 function volume_sphere() {
     //Write your code here
-  
+	let radius = Number(document.getElementById("radius").value);
+	let vol = document.getElementById("volume");
+	vol.value = Number(4*Math.PI*radius*radius*radius/3);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
